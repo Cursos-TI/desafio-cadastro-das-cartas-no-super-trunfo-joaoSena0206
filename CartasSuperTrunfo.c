c51fb68dc0cfa88eee78d8d14432d4dbb721ee69
João Sena <joaosena0206@gmail.com>
2025-02-19 16:03:21 -0300

    feat: adiciona nível mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
 
+int mostraResultadoComparacao(char atributo[], int carta)
+{
+    int resultadoComparacao = 0;
+    
+    if (carta == 1)
+    {
+        resultadoComparacao = 1;
+    }
+
+    printf("\n%s: Carta %d venceu (%d)", atributo, carta, resultadoComparacao);
+}
+
+
 int main()
 {
     // Cria variáveis para armazenar os dados das duas cartas posteriormente
     char estado_carta1;
     char codigo_carta1[4];
     char cidade_carta1[100];
-    int populacao_carta1;
+    unsigned long int populacao_carta1;
     float area_carta1;
     float pib_carta1;
     int pontosTuristicos_carta1;
     float densidadePopulacional_carta1;
     float pibCapita_carta1;
+    float poder_carta1;
 
     char estado_carta2;
     char codigo_carta2[4];
@@ -22,6 +36,7 @@ int main()
     int pontosTuristicos_carta2;
     float densidadePopulacional_carta2;
     float pibCapita_carta2;
+    float poder_carta2;
 
     // Pega os dados da primeira carta digitados pelo usuário
     printf("Insira o estado da primeira carta (A a H): \n");
@@ -36,7 +51,7 @@ int main()
     scanf(" %99[^\n]", cidade_carta1);
 
     printf("Digite o número de habitandes da cidade: \n");
-    scanf("%d", &populacao_carta1);
+    scanf("%lu", &populacao_carta1);
 
     printf("Digite a área da cidade (km²): \n");
     scanf("%f", &area_carta1);
@@ -46,10 +61,6 @@ int main()
 
     printf("Digite a quantidade de pontos turísticos na cidade: \n");
     scanf("%d", &pontosTuristicos_carta1);
-
-    // Calcula a densidade populacional e o pib per capita
-    densidadePopulacional_carta1 = populacao_carta1 / area_carta1;
-    pibCapita_carta1 = pib_carta1 * 1000000000 / populacao_carta1;
 
     // Pega os dados da segunda carta
     printf("\nAgora você deverá digitar os dados da segunda carta \n\n");
@@ -78,12 +89,19 @@ int main()
     scanf("%d", &pontosTuristicos_carta2);
 
     // Calcula a densidade populacional e o pib per capita
+    densidadePopulacional_carta1 = populacao_carta1 / area_carta1;
+    pibCapita_carta1 = pib_carta1 * 1000000000 / populacao_carta1;
+
     densidadePopulacional_carta2 = populacao_carta2 / area_carta2;
     pibCapita_carta2 = pib_carta2 * 1000000000 / populacao_carta2;
 
-    printf("\nAgora serão mostrados os dados das duas cartas \n\n");
+    // Calcula o super poder
+    poder_carta1 = populacao_carta1 + area_carta1 + pib_carta1 + pontosTuristicos_carta1 + pibCapita_carta1 + 1 / densidadePopulacional_carta1;
+    poder_carta2 = populacao_carta2 + area_carta2 + pib_carta2 + pontosTuristicos_carta2 + pibCapita_carta2 + 1 / densidadePopulacional_carta2;
 
     // Mostra os dados das duas cartas na tela
+    printf("\nAgora serão mostrados os dados das duas cartas \n\n");
+
     printf("Carta 1: \n");
     printf("Estado: %c \n", estado_carta1);
     printf("Código: %s \n", codigo_carta1);
@@ -104,7 +122,73 @@ int main()
     printf("PIB: %.2f bilhões de reais \n", pib_carta2);
     printf("Número de Pontos Turísticos: %d \n", pontosTuristicos_carta2);
     printf("Densidade Populacional: %.2f hab/km² \n", densidadePopulacional_carta2);
-    printf("PIB per Capita: %.2f reais", pibCapita_carta2);
+    printf("PIB per Capita: %.2f reais \n\n", pibCapita_carta2);
+
+    // Compara os atributos das cartas e mostra quem ganhou na tela
+    printf("Agora será feita uma comparação entre as duas cartas \n");
+
+    if (populacao_carta1 > populacao_carta2)
+    {   
+        mostraResultadoComparacao("População", 1);
+    }
+    else
+    {
+        mostraResultadoComparacao("População", 2);
+    }
+
+    if (area_carta1 > area_carta2)
+    {
+        mostraResultadoComparacao("Área", 1);
+    }
+    else
+    {
+        mostraResultadoComparacao("Área", 2);
+    }
+
+    if (pib_carta1 > pib_carta2)
+    {
+        mostraResultadoComparacao("PIB", 1);
+    }
+    else
+    {
+        mostraResultadoComparacao("PIB", 2);
+    }
+
+    if (pontosTuristicos_carta1 > pontosTuristicos_carta2)
+    {
+        mostraResultadoComparacao("Pontos Turísticos", 1);
+    }
+    else
+    {
+        mostraResultadoComparacao("Pontos Turísticos", 2);
+    }
+
+    if (densidadePopulacional_carta1 < densidadePopulacional_carta2)
+    {
+        mostraResultadoComparacao("Densidade Populacional", 1);
+    }
+    else
+    {
+        mostraResultadoComparacao("Densidade Populacional", 2);
+    }
+
+    if (pibCapita_carta1 > pibCapita_carta2)
+    {
+        mostraResultadoComparacao("PIB per Capita", 1);
+    }
+    else
+    {
+        mostraResultadoComparacao("PIB per Capita", 2);
+    }
+
+    if (poder_carta1 > poder_carta2)
+    {
+        mostraResultadoComparacao("Super Poder", 1);
+    }
+    else
+    {
+        mostraResultadoComparacao("Super Poder", 2);
+    }
 
     return 0;
 }
